8c513fb771bd8fd013b54b7108797c41d0427387
galdeano90654 <90654@sistemas.frc.utn.edu.ar>
2025-03-05 22:35:04 -0300

    segundo-paso

--- tamadoro/src/estudio/estudio.ts
+++ tamadoro/src/estudio/estudio.ts
@@ -1,11 +1,15 @@
-function temporizador1(){
-    let cont = 0
-    setTimeout(()=>{
-        console.log("ejecutando Set timeout");
-    }, 3000);
-    setInterval(()=>{
-        console.log("ejecutando set interval" + cont); ++cont;
-    }, 1000)
+function temporizador1() {
+    let cont = 3600;
+    const intervalo = setInterval(() => {
+        let minutos = Math.floor(cont / 60);
+        let segundos = (cont % 60).toString().padStart(2, '0'); 
+        console.log(`Ejecutando setInterval ${minutos}:${segundos}`);
+        
+        if (--cont < 0) {
+            clearInterval(intervalo);
+            console.log("Temporizador finalizado");
+        }
+    }, 1000);
 }
 
 export default temporizador1
